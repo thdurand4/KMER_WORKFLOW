9103a6dfa6e49713683b2b5d2bf720b82ac3ee25
thdurand4 <91953714+thdurand4@users.noreply.github.com>
2022-09-19 09:16:38 +0200

    Update name_columns.py

--- scripts/name_columns.py
+++ scripts/name_columns.py
@@ -3,23 +3,25 @@ import os
 import re
 import click
 
-@click.command(context_settings={'help_option_names': ('-h', '--help'), "max_content_width": 800})
+
+#  Ce script permet d'ajouter le nom des colonnes en utilisant le nom de fichier des accessions
+#  Pour lancer le script : python name_columns.py -in table_accession -o table_accession_named.
+
+@click.command(context_settings={'help_option_names': ('-h', '--help'), "max_content_width": 800})  # Utilisation de la librairie click pour définir les arguments du script
 @click.option('--input', '-in', default=None,
               type=click.Path(exists=True, file_okay=True, dir_okay=False, readable=True, resolve_path=True),
               required=True, show_default=True, help='Path to input accession file sorted')
 @click.option('--output', '-o', default=None,
               type=click.Path(exists=False, file_okay=True, dir_okay=False, readable=True, resolve_path=True),
               required=True, show_default=True, help='Path to output accession with columns name')
-
-
-def main (input, output):
+def main(input, output):
     """This programme add accession columns name of accession table sorted"""
-    acces_file = os.path.basename(input)
-    acces_name = re.sub("_20M_coverage_sorted.tbl","",acces_file)
-    df = pd.read_table(input, header=None)
-    df.columns = ["KMER",acces_name]
-    df.to_csv(output,sep="\t",index=False)
+    acces_file = os.path.basename(input)  # Récupération du nom des fichiers
+    acces_name = re.sub("_20M_coverage_sorted.tbl", "", acces_file)  # Remplacer la fin du nom par rien (ne garder que le nom de l'accession) et stocker ce nom dans une variable
+    df = pd.read_table(input, header=None)  # Lecture de la table qui ne possède pas encore de nom de colonnes
+    df.columns = ["KMER", acces_name]  # Définition du nom des colones
+    df.to_csv(output, sep="\t", index=False)  # Écriture de la nouvelle table avec des colonnes nommées dans un fichier
 
-if __name__ == '__main__':
+
+if __name__ == '__main__':  # Fonction permettant de lancer le script
     main()
-

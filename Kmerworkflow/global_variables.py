5f529ff52cb9841f41fe8a6ab9312eae209666cd
thdurand4 <91953714+thdurand4@users.noreply.github.com>
2023-06-27 09:01:12 +0200

    Update global_variables.py

--- Kmerworkflow/global_variables.py
+++ Kmerworkflow/global_variables.py
@@ -4,10 +4,8 @@ DOCS = "https://kmer-workflow.readthedocs.io/en/latest/"
 GIT_URL = "https://github.com/thdurand4/KMER_WORKFLOW"
 
 INSTALL_PATH = Path(__file__).resolve().parent
-SINGULARITY_URL_FILES = [('https://itrop.ird.fr/culebront_utilities/singularity_build/Singularity.culebront_tools.sif',
-                          'INSTALL_PATH/containers/Singularity.culebront_tools.sif'),
-                         ('https://itrop.ird.fr/culebront_utilities/singularity_build/Singularity.report.sif',
-                          'INSTALL_PATH/containers/Singularity.report.sif')
+SINGULARITY_URL_FILES = [('https://github.com/thdurand4/Docker_depot/blob/main/Dockerfile',
+                          'INSTALL_PATH/containers/Dockerfile')
                          ]
 
 DATATEST_URL_FILES = ("https://itrop.ird.fr/culebront_utilities/Data-Xoo-sub.zip", "Data-Xoo-sub.zip")

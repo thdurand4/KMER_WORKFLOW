3f8bd2c1358eea3f39fae9ab25017f67bc626ee0
thdurand4 <91953714+thdurand4@users.noreply.github.com>
2023-06-28 08:39:13 +0200

    Update global_variables.py

--- Kmerworkflow/global_variables.py
+++ Kmerworkflow/global_variables.py
@@ -7,7 +7,7 @@ INSTALL_PATH = Path(__file__).resolve().parent
 SINGULARITY_URL_FILES = [('https://github.com/thdurand4/Docker_depot/blob/main/Dockerfile_tools',
                           'INSTALL_PATH/containers/Dockerfile_tools'),
                          ('https://github.com/thdurand4/Docker_depot/blob/main/Dockerfile_perl',
-                          'INSTALL_PATH/containers/Dockerfile_perl'
+                          'INSTALL_PATH/containers/Dockerfile_perl')
                          ]
 
 DATATEST_URL_FILES = ("https://itrop.ird.fr/culebront_utilities/Data-Xoo-sub.zip", "Data-Xoo-sub.zip")
